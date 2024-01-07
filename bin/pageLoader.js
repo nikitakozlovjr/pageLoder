fcbb1237d6b1fbc4209f3b7de034cc59eb8707a5
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-08 00:16:01 +0300

    Correct get option

--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import pageloader from '../src/index.js';
 
 program
     .name('pageloader')
     .description('Page loader utility')
     .version('1.0.0')
-    .option('-o, --output <dir>', 'output dir (default: "/home/user/current-dir")')
+    .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir")')
     .argument('<url>')
-    .action((__url) => {
-        console.log(`Go! -> ${program.opts().format}`);
+    .action(async (url) => {
+        await pageloader(url, program.opts().output);
     })
 
 program.parse();
