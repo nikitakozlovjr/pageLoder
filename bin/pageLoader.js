a1e77eda7283400affab4cc1e293203bd5a24c36
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-07 19:40:59 +0300

    Write commander function

--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+    .name('pageloader')
+    .description('Page loader utility')
+    .version('1.0.0')
+    .option('-o, --output <dir>', 'output dir (default: "/home/user/current-dir")')
+    .argument('<url>')
+    .action((__url) => {
+        console.log(`Go! -> ${program.opts().format}`);
+    })
+
+program.parse();
