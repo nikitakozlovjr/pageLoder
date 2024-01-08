aed3ec92aeb83eadef10c5323e7d41c83bae6b72
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-09 02:11:35 +0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import getData from "./httpRequest/getData.js";
-import getFilename from './jobfilepath/getFilename.js';
-import writeData from './jobfilepath/writeData.js';
-import path from 'path';
+import createFileWithData from './jobfilepath/index.js';
 
 const pageloader = async (url, dirname = process.cwd()) => {
     const data = await getData(url);
-    const pathToWrite = dirname === process.cwd() 
-                    ? path.join(dirname, getFilename(url)) 
-                    : path.join(process.cwd(), dirname, getFilename(url));
-    await writeData(pathToWrite, data);
+    await createFileWithData(dirname, url, data)
 };
 
 export default pageloader;
