a7734306c946c1b8e33955fce51aae292fe59df3
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-11 22:22:14 +0300

    Add return path to loaded file

--- src/jobfilepath/index.js
+++ src/jobfilepath/index.js
@@ -4,6 +4,7 @@ import writeData from './writeData.js'
 const createFileWithData = async (dirname, url, data) => {
     const pathToWrite = buildPathToWrite(dirname, url);
     await writeData(pathToWrite, data);
+    console.log(pathToWrite);
 };
 
 export default createFileWithData;
