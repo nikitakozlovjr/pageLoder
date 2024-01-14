bdcd1db663fb327e1af90086513dd1ce5d8dc466
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-14 19:02:47 +0300

    Correct get name file

--- src/img/getFilename.js
+++ src/img/getFilename.js
@@ -1,12 +1,14 @@
 import path from 'node:path';
 
-const getFilename = (src, url = '') => {
+const getFilename = (src) => {
     const extname = path.extname(src);
-    const firstPathFilename = src.slice(0, src.length - extname.length).split('')
+    const filename = src.slice(0, src.length - extname.length).split('')
                         .map((el) => /[^a-z^0-9]/.test(el) ? '-' : el)
                         .join('')
+                        .slice(1)
                         .concat(extname); 
-    return {extname, firstPathFilename};
+    return filename;
 };
+
 
 export default getFilename;
