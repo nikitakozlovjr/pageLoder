adccff967178b3612ad214e548a03b1227011616
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-24 11:12:49 +0300

    Refactoring

---
+++ src/buildPath/buildPathToWrite.js
@@ -0,0 +1,10 @@
+import getFilename from './getFilename.js';
+import path from 'node:path';
+
+const buildPathToWrite = (dirname, url) => {
+    return dirname === process.cwd() 
+                    ? path.join(dirname, getFilename(url)) 
+                    : path.join(process.cwd(), dirname, getFilename(url));
+};
+
+export default buildPathToWrite;
