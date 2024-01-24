5f94a1e3207c6705ad485069ba573583ca9606f3
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-24 11:11:20 +0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import getData from "./html/getData.js";
-import createFileWithData from './html/index.js';
+import Data from "./getData.js";
+import axios from "axios";
+import fsp from 'fs/promises';
+import buildPathToWrite from "./buildPath/buildPathToWrite.js";
+
 
 const pageloader = async (url, dirname = process.cwd()) => {
-    const data = await getData(url);
-    await createFileWithData(dirname, url, data)
+    const dataReturn = new Data(axios);
+    const data = await dataReturn.getHtml(url);
+    await fsp.writeFile(buildPathToWrite(dirname, url), data)
 };
 
 export default pageloader;
